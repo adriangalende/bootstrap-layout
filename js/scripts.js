d208c2a42c1c6e2ca9fd6a24a4e48488d16da635
Adrián <adriangalende@gmail.com>
2019-02-21 21:09:18 +0100

    home 98%

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-// Empty JS for your own code to be here
+
+var mymap = L.map('myMap').setView([39.5688565, 2.6652431], 12);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYWdhbGVuZGUiLCJhIjoiY2pzZXp2aWtoMGE3NjQ0bzJ0cmZvdGhzYiJ9.C_2fZGDSvc1u95gmp2Bf2Q'
+}).addTo(mymap);
+
+var marker = L.marker([39.5688565, 2.6652431]).addTo(mymap);
